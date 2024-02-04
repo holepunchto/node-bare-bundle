12795d198213d4aa25e84b68ac3cc63c78964ac2
Mathias Buus <mathiasbuus@gmail.com>
2024-02-04 11:06:53 +0100

    fix mb

--- test/test.js
+++ test/test.js
@@ -4,38 +4,22 @@ const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
 
-test.solo('can run bundle', async function (t) {
+const BUNDLE = require('./fixtures/bundle.js')
+
+test('can run bundle', async function (t) {
   t.plan(1)
 
-  const bundle = fs.readFileSync(path.join(__dirname, 'streamx.data'))
-  console.log(crypto.createHash('sha256').update(bundle).digest('hex'))
+  const stream = runBundle(BUNDLE, '/test.js')
 
-  {
-    const bundle = await fs.promises.readFile(path.join(__dirname, 'streamx.data'))
-    console.log(crypto.createHash('sha256').update(bundle).digest('hex'))
-    console.log(await fs.promises.stat(path.join(__dirname, 'streamx.data')))
-  }
-
-  try {
-    const stream = runBundle(bundle, '/test.js')
-
-    stream.on('data', function (data) {
-      t.is(data, 'works!')
-    })
-  } catch (err) {
-    console.log(err)
-    t.ok(true, 'whatever')
-  }
-
-  console.log(crypto.createHash('sha256').update(bundle).digest('hex'))
+  stream.on('data', function (data) {
+    t.is(data, 'works!')
+  })
 })
 
 test('can run any part of the bundle', function (t) {
   t.plan(1)
 
-  const bundle = fs.readFileSync(path.join(__dirname, 'streamx.bundle'))
-
-  const streamx = runBundle(bundle, '/node_modules/streamx/index.js')
+  const streamx = runBundle(BUNDLE, '/node_modules/streamx/index.js')
 
   t.ok(streamx.Readable)
 })
