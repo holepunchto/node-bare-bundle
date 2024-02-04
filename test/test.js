064e9b360a9dea43c6e7ecddba93e9c6e8775f2b
Mathias Buus <mathiasbuus@gmail.com>
2024-02-04 10:59:57 +0100

    more info

--- test/test.js
+++ test/test.js
@@ -4,11 +4,17 @@ const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
 
-test.solo('can run bundle', function (t) {
+test.solo('can run bundle', async function (t) {
   t.plan(1)
 
   const bundle = fs.readFileSync(path.join(__dirname, 'streamx.data'))
   console.log(crypto.createHash('sha256').update(bundle).digest('hex'))
+
+  {
+    const bundle = await fs.promises.readFile(path.join(__dirname, 'streamx.data'))
+    console.log(crypto.createHash('sha256').update(bundle).digest('hex'))
+    console.log(await fs.promises.stat(path.join(__dirname, 'streamx.data')))
+  }
 
   try {
     const stream = runBundle(bundle, '/test.js')
